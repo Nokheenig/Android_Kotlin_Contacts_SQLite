78847c4e3ef9f75a2a6fd07d831bdc4ddfbd881b
Yoann <yoann.chamillard@gmail.com>
2024-02-13 14:41:34 +0100

    191.SQL 2

--- app/src/main/java/com/example/contacts/data/DatabaseHandler.kt
+++ app/src/main/java/com/example/contacts/data/DatabaseHandler.kt
@@ -5,13 +5,19 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.contacts.utils.Constants
 
-class DatabaseHandler (val context: Context):
+class DatabaseHandler (context: Context):
     SQLiteOpenHelper(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION) {
-    override fun onCreate(p0: SQLiteDatabase?) {
-        //TODO("Not yet implemented")
+    override fun onCreate(db: SQLiteDatabase?) {
+        val CREATE_TABLE_CONTACTS = "CREATE TABLE ${Constants.TABLE_NAME} (" +
+                "${Constants.KEY_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "${Constants.KEY_NAME} TEXT," +
+                "${Constants.KEY_PHONE_NUMBER} TEXT)"
+        db?.execSQL(CREATE_TABLE_CONTACTS)
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        //TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ${Constants.TABLE_NAME}")
+
+        onCreate(db)
     }
 }
