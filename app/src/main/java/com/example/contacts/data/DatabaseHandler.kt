d6f78e408288c677683c2e234a49dfb53d0a45f4
Yoann <yoann.chamillard@gmail.com>
2024-02-13 15:09:28 +0100

    192.SQL 3 - Added addContact and getContact functions

--- app/src/main/java/com/example/contacts/data/DatabaseHandler.kt
+++ app/src/main/java/com/example/contacts/data/DatabaseHandler.kt
@@ -1,8 +1,10 @@
 package com.example.contacts.data
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.contacts.models.Contact
 import com.example.contacts.utils.Constants
 
 class DatabaseHandler (context: Context):
@@ -20,4 +22,34 @@ class DatabaseHandler (context: Context):
 
         onCreate(db)
     }
+
+    /*
+    CRUD Operations - Create, Read, Update, Delete
+     */
+    fun addContact(contact: Contact) {
+        val db = this.writableDatabase
+
+        val values = ContentValues()
+        values.put(Constants.KEY_NAME, contact.name)
+        values.put(Constants.KEY_PHONE_NUMBER, contact.phoneNumber)
+
+        //insert to row
+        db.insert(Constants.TABLE_NAME, null, values)
+        db.close()
+    }
+
+    fun getContact(id: Int): Contact {
+        val db = this.readableDatabase
+
+        val cursor = db.query(Constants.TABLE_NAME,
+            arrayOf(Constants.KEY_ID, Constants.KEY_NAME, Constants.KEY_PHONE_NUMBER),
+            Constants.KEY_ID + "=?", arrayOf(id.toString()), null, null, null, null )
+
+        cursor?.let {
+            it.moveToFirst()
+        }
+
+        return  Contact(cursor.getString(0).toInt(), cursor.getString(1), cursor.getString(2))
+    }
+
 }
--- app/src/main/java/com/example/contacts/models/Contact.kt
+++ app/src/main/java/com/example/contacts/models/Contact.kt
@@ -1,5 +1,5 @@
 package com.example.contacts.models
 
-data class Contacts (val id: Int, val name:String, val phoneNumber: String){
+data class Contact (val id: Int, val name:String, val phoneNumber: String){
     constructor(name: String, phoneNumber: String): this(-1, name, phoneNumber)
 }
